2dff4f0578634fc6f73c1665204a2e081ee43a07
poonamslac <145401050+poonamslac@users.noreply.github.com>
2024-03-11 23:03:34 -0700

    Made downtime entry work
    Also, added comments display to Shift Calendar and other changes.

--- src/pages/ShiftCalendar/ShiftCalendar.js
+++ src/pages/ShiftCalendar/ShiftCalendar.js
@@ -1,16 +1,13 @@
 // ShiftCalendar.js
 import React, { useState, useEffect } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
+//import { Button, Modal, Form } from 'react-bootstrap';
 import DataTable from './DataTable.js'; // Import the DataTable component
-import moment from 'moment';
-import DatePicker from 'react-datepicker';
+//import moment from 'moment';
+//import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 function ShiftCalendar() {
   const [shiftdata, setShiftCal] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
 
   useEffect(() => {
     getShiftCal();
@@ -30,96 +27,9 @@ function ShiftCalendar() {
       .then((data) => setShiftCal(data));
   }
 
-  async function createShiftCal() {
-    fetch('/api/aosd-shift-reporting-backend/shiftdata', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        start_time: moment.tz(startTime, 'America/Los_Angeles').toISOString(),
-        end_time: moment.tz(endTime, 'America/Los_Angeles').toISOString(),
-      }),
-    })
-
-      .then((response) => response.text())
-      .then((data) => {
-        alert(data);
-        getShiftCal(); // Refresh data after adding shift
-        handleCloseModal();
-      });
-  }
-
-  async function deleteShiftCal() {
-    let id = prompt('Enter Shift Id');
-    fetch(`/api/aosd-shift-reporting-backend/shiftdata/${id}`, {
-      method: 'DELETE',
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        alert(data);
-        getShiftCal(); // Refresh data after deleting shift
-      });
-  }
-
-  const handleShowModal = () => setShowModal(true);
-  const handleCloseModal = () => {
-    setShowModal(false);
-    // Clear the input fields when the modal is closed
-    setStartTime('');
-    setEndTime('');
-  };
-
  return (
     <div>
-      <Button variant="primary" onClick={handleShowModal} style={{ margin: '5px' }}>
-        Add Shift
-      </Button>
-      <Button variant="danger" onClick={deleteShiftCal} style={{ margin: '5px' }}>
-        Delete Shift
-      </Button>
-
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add Shift</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="startTime">
-              <Form.Label>Start Time:</Form.Label>
-              <DatePicker
-                selected={startTime}
-                onChange={(date) => setStartTime(date)}
-                showTimeSelect
-                dateFormat="Pp"
-                placeholderText="Select Date and Time"
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="endTime">
-              <Form.Label>End Time:</Form.Label>
-              <DatePicker
-                selected={endTime}
-                onChange={(date) => setEndTime(date)}
-                showTimeSelect
-                dateFormat="Pp"
-                placeholderText="Select Date and Time"
-                required
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={createShiftCal}>
-            Add Shift
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-      {shiftdata ? (
+        {shiftdata ? (
         <DataTable key={JSON.stringify(shiftdata)} data={shiftdata} />
       ) : (
         'There is no Shift data available'
--- src/pages/ShiftComments/ShiftInfo.jsx
+++ src/pages/ShiftComments/ShiftInfo.jsx
@@ -1,11 +1,14 @@
 // ShiftInfo.jsx
-// Front end for entering Shift Data
+// Front end for entering Shift Reporting data and any Downtime entries.
+// Dates needed formatting to local timestamp, or else it was converting into UTC.
 import React, { useState, useEffect } from 'react';
 import './DowntimeEntry.css'; // Import the CSS file
 import axios from 'axios';
 import moment from 'moment';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { Button, Form, Col, Row, Container } from 'react-bootstrap';
+
 
 function ShiftInfo() {
   const [shiftDates, setShiftDates] = useState({
@@ -15,7 +18,7 @@ function ShiftInfo() {
 
 
   const [commentsData, setCommentsData] = useState([{ comments: '' }]);
-
+// Category needs to be a dropdown selectable list
   const staticCategory = ["Configuration Change", "Tuning", "Accelerator Down", "User Off", "Scheduled Off"];
   
 // Store the selected system_id in the form data
@@ -23,7 +26,7 @@ function ShiftInfo() {
     { description: '', category: '', selectedSystemId: '', 
       start: null, end: null, caterId: '', elogUrl: ''},
   ]);
-
+// Use the ACCEL_SYSTEMS module to retrieve system_id and system_name
   const [accelsystem, setAccelSystem] = useState([]);
 
   // Fetch the list of systems when the component mounts
@@ -55,6 +58,17 @@ function ShiftInfo() {
   const handleDowntimeChange = (index, fieldName, value) => {
     const updatedDowntimeData = [...downtimeData];
     updatedDowntimeData[index] = { ...updatedDowntimeData[index], [fieldName]: value };
+
+    console.log('Updated Downtime Data:', updatedDowntimeData);
+
+  // Find the selected system and set its system_id
+  const selectedSystem = accelsystem.find(system => system.system_name === value);
+  if (selectedSystem) {
+    console.log('Selected System:', selectedSystem);
+
+    updatedDowntimeData[index].selectedSystemId = selectedSystem.system_id;
+  }
+
     setDowntimeData(updatedDowntimeData);
   };
 
@@ -88,11 +102,24 @@ function ShiftInfo() {
       endTime: moment(shiftDates.endTime).format('YYYY-MM-DDTHH:mm:ss'),
     };   
 
+    console.log("Axios Request Payload:", {
+      shiftDates: formattedShiftDates,
+      commentsData,
+      downtimeData: downtimeData.map(entry => ({
+        ...entry,
+        system_id: entry.selectedSystemId,
+      })),
+    });
+    
     axios
       .post('/api/aosd-shift-reporting-backend/shiftDates', {
         shiftDates: formattedShiftDates,
         commentsData,
-        downtimeData,
+        // downtimeData,
+        downtimeData: downtimeData.map(entry => ({
+          ...entry,
+          system_id: entry.selectedSystemId, // Include the system_id in the request
+        })),
       })
       .then((response) => {
         console.log('Response:', response);
@@ -107,67 +134,83 @@ function ShiftInfo() {
       });
   }
 
-  return (
-    <div>
-      <h1>Shift Reporting Form</h1>
-      <form onSubmit={createShiftInfo}>
-        <label>
-          Start Time:
-          <DatePicker
-            selected={shiftDates.startTime}
-            onChange={(date) => handleShiftChange('startTime', date)}
-            showTimeSelect
-            dateFormat="Pp"
-            required
-          />
-        </label>
-        <br />
-        <label>
-          End Time:
-          <DatePicker
-            selected={shiftDates.endTime}
-            onChange={(date) => handleShiftChange('endTime', date)}
-            showTimeSelect
-            dateFormat="Pp"
-            required
-          />
-        </label>
-        <br /><br />
-        {commentsData.map((comments, index) => (
-          <div key={index}>
-            <label>
-              Comment:
-	           <textarea
-                name="comments"
-                value={comments.comments}
-                onChange={(e) => handleCommentChange(index, e)}
+
+
+return (
+  <Container>
+    <h1>Shift Reporting Form</h1>
+    <Form onSubmit={createShiftInfo}>
+    <Row>
+          <Col>
+            <Form.Group controlId="startTime">
+              <Form.Label>Start Time:</Form.Label>
+              <DatePicker
+                selected={shiftDates.startTime}
+                onChange={(date) => handleShiftChange('startTime', date)}
+                showTimeSelect
+                dateFormat="Pp"
                 required
-              className="expandable-textarea"
               />
-            </label>
-            <button type="button" onClick={() => handleRemoveComment(index)}>
-              Remove Comment
-            </button>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group controlId="endTime">
+              <Form.Label>End Time: </Form.Label>
+              <DatePicker
+                selected={shiftDates.endTime}
+                onChange={(date) => handleShiftChange('endTime', date)}
+                showTimeSelect
+                dateFormat="Pp"
+                required
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        {commentsData.map((comments, index) => (
+          <div key={index} className="comment-container">
+            <div className="comment-row">
+              <Form.Group controlId={`comment${index}`} className="comment-group">
+                <Form.Label>Comment:</Form.Label>
+                <div className="comment-controls">
+                  <Form.Control
+                    as="textarea"
+                    name="comments"
+                    value={comments.comments}
+                    onChange={(e) => handleCommentChange(index, e)}
+                    required
+                    className="comment-textarea"
+                  />
+                  <Button variant="danger" onClick={() => handleRemoveComment(index)}>
+                    Remove Comment
+                  </Button>
+                </div>
+              </Form.Group>
+            </div>
           </div>
         ))}
-        <button type="button" onClick={handleAddComment}>
-          Add Comment
-        </button>
-        <br />
-        <h2>Downtime Entry</h2>
-        <div className="downtime-header-row">
-          <div className="downtime-header-label">Description</div>
+
+
+      <Button variant="primary" onClick={handleAddComment}>
+        Add Comment
+      </Button>
+<hr/>
+      <h2>Downtime Entry</h2>
+      <div className="downtime-header-row">
+        <div className="downtime-header-label">Description</div>
           <div className="downtime-header-label">Category</div>
           <div className="downtime-header-label">System</div>
-          <div className="downtime-header-label">Start</div>
-          <div className="downtime-header-label">End</div>
+          <div className="downtime-header-label">Start Date/ Time</div>
+          <div className="downtime-header-label">End Date/Time </div>
           <div className="downtime-header-label">Cater ID</div>
-          <div className="downtime-header-label">Elog URL</div>
-        </div>
+          <div className="downtime-header-label ">Elog URL</div>
+      </div>
 
-        {downtimeData.map((downtime, index) => (
-          <div key={index} className="downtime-data-row">
-	        <textarea
+      {downtimeData.map((downtime, index) => (
+        <div key={index} className="downtime-data-row">
+          <textarea
             name="description"
             value={downtime.description}
             onChange={(e) => handleDowntimeChange(index, 'description', e.target.value)}
@@ -197,7 +240,7 @@ function ShiftInfo() {
             ))}
           </select>
 
-            <DatePicker
+          <DatePicker
               selected={downtime.start}
               onChange={(date) => handleDowntimeChange(index, 'start', date)}
               showTimeSelect
@@ -205,8 +248,8 @@ function ShiftInfo() {
               placeholderText="Select Date and Time"
               required
               className='date-height'
-            />
-    	      <DatePicker
+          />
+    	    <DatePicker
                 selected={downtime.end}
                 onChange={(date) => handleDowntimeChange(index, 'end', date)}
                 showTimeSelect
@@ -214,32 +257,34 @@ function ShiftInfo() {
                 placeholderText="Select Date and Time"
                 required
                 className='date-height'
-                />
-              <input
+          />
+          <input
                 type="text"
                 name="caterId"
                 value={downtime.caterId}
                 onChange={(e) => handleDowntimeChange(index, 'caterId', e.target.value)}
-              />
-              <input
+          />
+          <input
                 type="text"
                 name="elogUrl"
                 value={downtime.elogUrl}
                 onChange={(e) => handleDowntimeChange(index, 'elogUrl', e.target.value)}
-              />
-            <button type="button">
-              Add Program
-            </button>
-          </div>
-        ))}
-        <button type="button" onClick={handleAddDowntime}>
-          Add Downtime Entry
-        </button>
-        <br />
-        <button type="submit">Submit Shift Data</button>
-      </form>
-    </div>
-  );
+          />
+            <Button variant="danger" onClick={() => handleRemoveDowntime(index)}>
+               Remove Row
+            </Button>
+        </div>
+      ))}
+      <Button variant="primary" onClick={handleAddDowntime}>
+        Add Downtime Entry
+      </Button><br/>
+      <hr/>
+      <Button variant="success" type="submit" className="submitShift">
+        Submit Shift Data
+      </Button>
+    </Form>
+  </Container>
+);
 }
 
 export default ShiftInfo;
